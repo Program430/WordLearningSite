a4fddb27f66a23b034515fbf0301582a8e94988c
Timur <abdtimurrif@gmail.com>
2024-10-17 02:48:27 +0300

    HTML, CSS, JS, in term of base functions, was added

--- english_words_site/main/urls.py
+++ english_words_site/main/urls.py
@@ -1,7 +1,10 @@
 
 from django.urls import path
-from main.views import home
+from main.views import login, word_list, word_card, user_word_list
 
 urlpatterns = [
-    path('', home, name= 'home'),
+    path('login/', login, name= 'login'),
+    path('word_list/', word_list, name= 'word_list'),
+    path('word_card/', word_card, name= 'word_card'),
+    path('user_word_list/', user_word_list, name= 'user_word_list'),
 ]
--- english_words_site/main/views.py
+++ english_words_site/main/views.py
@@ -1,5 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def home(request):
-    return HttpResponse('Hello')
+def login(request):
+    return render(request, 'main/login_register/login_register.html')
+
+
+
+
+
+def word_list(request):
+    return render(request, 'main/word_list/main_word_list.html')
+
+def word_card(request):
+    return render(request, 'main/card/word_card.html')
+
+def user_word_list(request):
+    return render(request, 'main/user_word_list/user_word_list.html')
